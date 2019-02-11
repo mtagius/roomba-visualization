25034ab8dd43659214c169e7ac265ba8bda05e6d
Matthew Agius <mtagius@gmail.com>
2019-02-11 01:09:19 -0500

    map now auto resizes to fit screen

--- sketch.js
+++ sketch.js
@@ -5,8 +5,9 @@ border = 20;
 XandYmin = border;
 Xmax = null;
 Ymax = null;
-dotSize = 15;
+dotSize = 5;
 coordinates = [];
+screenCoordinates = [];
 
 
 function createPoint() {
@@ -40,15 +41,15 @@ function setup() {
 function draw() {
   background("#f0f9f0");
 
-  if(frameCount++ >= 15 && coordinates.length < 100) {
+  if(frameCount++ >= 15) {
     frameCount = 1;
     //createPoint();
     $.ajax({
       url: "http://localhost:3000/api/local/info/mission",
       type: "GET",
       success: function(result) {
-        x = result.pose.point.x + Math.floor(windowWidth / 2);
-        y = result.pose.point.y + Math.floor(windowHeight / 2);
+        x = result.pose.point.x;
+        y = result.pose.point.y;
         if(coordinates.length > 0) {
           if(x == coordinates[coordinates.length - 1][0] && y == coordinates[coordinates.length - 1][1]) {
 
@@ -66,14 +67,98 @@ function draw() {
     });
   }
 
+  var lowestX = 0;
+  var lowestY = 0;
+
+  screenCoordinates = [];
+
   for(i = 0; i < coordinates.length; i++) {
+    if(coordinates[i][0] < lowestX) {
+      lowestX = coordinates[i][0];
+    }
+    if(coordinates[i][1] < lowestY) {
+      lowestY = coordinates[i][1];
+    }
+  }
+
+  for(i = 0; i < coordinates.length; i++) {
+    screenCoordinates.push([coordinates[i][0] + abs(lowestX) + 1, coordinates[i][1] + abs(lowestY) + 1]);
+  }
+
+  var highestX = 0;
+  var highestY = 0;
+
+
+  for(i = 0; i < screenCoordinates.length; i++) {
+    if(screenCoordinates[i][0] > highestX) {
+      highestX = screenCoordinates[i][0];
+    }
+    if(screenCoordinates[i][1] > highestY) {
+      highestY = screenCoordinates[i][1];
+    }
+  }
+
+  var xRatio = false;
+  var yRatio = false;
+
+  if((windowWidth - (2 * border)) < highestX) {
+    xRatio = true;
+  }
+  if((windowHeight - (2 * border)) < highestY) {
+    yRatio = true;
+  }
+
+  if(xRatio && yRatio) {
+    if(((windowWidth - (2 * border)) - highestX) < ((windowHeight - (2 * border)) - highestY)) {
+      xRatio = true;
+      yRatio = false;
+    } else {
+      xRatio = false;
+      yRatio = true;
+    }
+  }
+
+
+  for(i = 0; i < screenCoordinates.length; i++) {
+    if(xRatio) {
+      xRatio = (windowWidth - (2 * border)) / highestX;
+      screenCoordinates[i][0] = Math.floor(lerp(0, (windowWidth - (2 * border)), screenCoordinates[i][0] / highestX)) + border;
+      screenCoordinates[i][1] = Math.floor(screenCoordinates[i][1] * xRatio) + border;
+    } else if (yRatio) {
+      yRatio = (windowHeight - (2 * border)) / highestY;
+      screenCoordinates[i][0] = Math.floor(screenCoordinates[i][0] * yRatio) + border;
+      screenCoordinates[i][1] = Math.floor(lerp(0, (windowHeight - (2 * border)), screenCoordinates[i][1] / highestY)) + border;
+    } else {
+      screenCoordinates[i][0] += border;
+      screenCoordinates[i][1] += border;
+    }
+  }
+
+  highestX = 0;
+  highestY = 0;
+
+  for(i = 0; i < screenCoordinates.length; i++) {
+    if(screenCoordinates[i][0] > highestX) {
+      highestX = screenCoordinates[i][0];
+    }
+    if(screenCoordinates[i][1] > highestY) {
+      highestY = screenCoordinates[i][1];
+    }
+  }
+
+  for(i = 0; i < screenCoordinates.length; i++) {
+    screenCoordinates[i][0] += Math.floor(((windowWidth - (2 * border)) - highestX) / 2);
+    screenCoordinates[i][1] += Math.floor(((windowHeight - (2 * border)) - highestY) / 2);
+  }
+
+  for(i = 0; i < screenCoordinates.length; i++) {
     fill("#40493B");
     noStroke();
-    ellipse(coordinates[i][0], coordinates[i][1], dotSize, dotSize);
+    //ellipse(screenCoordinates[i][0], screenCoordinates[i][1], dotSize, dotSize);
     if(i > 0) {
       stroke("#40493B");
       strokeWeight(3);
-      line(coordinates[i - 1][0], coordinates[i - 1][1], coordinates[i][0], coordinates[i][1]);
+      line(screenCoordinates[i - 1][0], screenCoordinates[i - 1][1], screenCoordinates[i][0], screenCoordinates[i][1]);
     }
   }
 }
